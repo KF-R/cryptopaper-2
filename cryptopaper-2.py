bcd920d15fd6e6653fe97bb7e2e82a09e932be89
KF-R <6677966+KF-R@users.noreply.github.com>
2024-06-18 04:22:01 -0400

    2.0.1 - Options

--- cryptopaper-2.py
+++ cryptopaper-2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 ## (c) 2024 Kerry Fraser-Robinson
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, render_template
 from flask_cors import CORS
 import requests
-import os, sys, time, string
+import os, sys, time, string, re
 from werkzeug.utils import secure_filename    
 
 from bs4 import BeautifulSoup                   # For BBC News scraping proxy
@@ -11,16 +11,29 @@ import socket                                   # For confirming ip address
 import base64                                   # For parsing image uploads
 
 PORT = 5000
-LOCATION = 'Toronto'
+DEFAULT_LOCATION = 'Toronto'
+location = DEFAULT_LOCATION
 
-TITLE, VERSION = 'Cryptopaper', '2.0.0'
+TITLE, VERSION = 'Cryptopaper', '2.0.1'
 LIBDIR = 'lib/'
 T_START = int(time.time())
+OPTIONS_FILE = 'options.txt'
+KEYWORDS_FILE = 'watch-words.txt'
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=LIBDIR)
 CORS(app)                                       # Enable CORS for all routes
 
-def print_log(log_string = '',log_to_file=True, noStdOut = True):
+def read_location():
+    global location
+    try:
+        with open(os.path.join(LIBDIR, OPTIONS_FILE), 'r') as file:
+            location = file.read().lstrip().rstrip()
+            print_log(f"Using location: `{location}`")
+    except:
+        location = DEFAULT_LOCATION
+        print_log(f"Using DEFAULT location (`{location}`)")
+
+def print_log(log_string = '',log_to_file=True, noStdOut = False):
     LOG_FILENAME = sys.argv[0].split('.')[0] + '.log'
 
     timestamp = time.strftime("%m/%d/%y  %H:%M:%S", time.localtime(time.time()))
@@ -49,8 +62,26 @@ def ip_address():
         with open(LOG_FILENAME, "a") as file:
             file.write(f"{newLogLine}\n")   
 
-@app.route('/')
+def sanitize_watch_words(watch_words: str):
+    lines = watch_words.split('\n')
+    # Remove non-alphanumeric characters and empty lines
+    sanitized_lines = [re.sub(r'\W+', '', line) for line in lines if line.strip() != '']
+    return '\n'.join(sorted(sanitized_lines, key=str.lower))
+
+def sanitize_location(location: str):
+    return ''.join(ch for ch in location if ch.isprintable())    
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    global location
+    if request.method == 'POST' and 'location' in request.form:
+        location = sanitize_location(request.form['location'])
+        try:
+            with open(os.path.join(LIBDIR, OPTIONS_FILE), 'w') as file:
+                file.write(location)        
+            print_log(f'{OPTIONS_FILE} (location) updated.')
+        except:
+            print_log('Location not updated.')
     return send_from_directory(LIBDIR, 'index.html')
 
 @app.route('/words', methods=['GET'])
@@ -58,7 +89,7 @@ def words():
     watchWords = []
     # Load watch words. Ignore any words containing non-printable characters  
     try: 
-        watchWords = [line.strip() for line in open(os.path.join(LIBDIR, 'watch-words.txt')) if all(char in string.printable for char in line)]
+        watchWords = [line.strip() for line in open(os.path.join(LIBDIR, KEYWORDS_FILE)) if all(char in string.printable for char in line)]
         print_log("Watch list loaded: '" + ", ".join(watchWords) + "'")
     except:
         # Defaults :
@@ -73,7 +104,7 @@ def status():
         'version': VERSION,
         'start_time': T_START,
         'ip_addr': ip_address(),
-        'location': LOCATION
+        'location': location
     }
     return jsonify(statusData)
 
@@ -88,6 +119,21 @@ def save_canvas():
         f.write(img_data)
     
     return jsonify({'status': 'success', 'file_path': file_path})
+
+@app.route('/options')
+def options():
+    system_name = os.uname().nodename
+    server_time = time.strftime("%m/%d/%y  %H:%M:%S", time.localtime(time.time()))
+    return render_template('options.html', status = f'{system_name} :: {server_time}')
+
+@app.route('/save_watch_words', methods=['POST'])
+def save_watch_words():
+    text = sanitize_watch_words(request.form['text'])
+    try:
+        with open(os.path.join(LIBDIR, KEYWORDS_FILE), 'w') as file:
+            file.write(text)
+        return {'message': 'File saved successfully.'}
+    except: return {'error': 'An error occurred while saving the watch-words file.'}
 
 @app.route('/<filename>.png')
 def serve_png(filename):
@@ -141,5 +187,5 @@ def fetch_bbc_news():
 
 if __name__ == '__main__':
     print_log(f"v{VERSION}: Initialising...")
-
+    read_location()
     app.run(debug=True, host='0.0.0.0', port=PORT, ssl_context='adhoc')
